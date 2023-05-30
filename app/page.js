c9b0933cd1addb078ce7921c528bddb654183fbc
Franck G <gutmann.franck@outlook.fr>
2023-05-30 09:14:17 +0200

    feat: add play page

--- app/page.js
+++ app/page.js
@@ -13,14 +13,12 @@ export default function Home() {
         </svg>
       </div>
 
-      <div class="tooltip" data-tip="Bientôt disponible">
-        <button class="btn btn-primary gap-2 btn-wide">
-          Jouer en ligne
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
-          </svg>
-        </button>
-      </div>
+      <a class="btn btn-primary gap-2 btn-wide" href="play">
+        Jouer en ligne
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+        </svg>
+      </a>
 
       <a className="btn btn-secondary gap-2" href="https://github.com/Livl-Corporation/livl-madness/releases">
         Télécharger
---
+++ app/play/page.js
@@ -0,0 +1,7 @@
+export default function PlayPage() {
+    return (
+        <p>
+            Coming soon
+        </p>
+    )
+}
