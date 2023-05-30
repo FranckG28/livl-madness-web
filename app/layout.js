d6c42812451c7e515e47842cef769a2a11d930c4
Franck G <gutmann.franck@outlook.fr>
2023-05-30 09:13:49 +0200

    feat: new homepage layout

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr" data-theme="emerald">
-      <body className={inter.className + " bg-slate-100"}>{children}</body>
+      <body className={inter.className + " bg-slate-100 h-screen"}>{children}</body>
     </html>
   )
 }
